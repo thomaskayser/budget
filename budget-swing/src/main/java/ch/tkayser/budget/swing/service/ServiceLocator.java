1916ce41112ffbd2bba1bdc486d993045c4a17bd
tom <thomas.kayser@anea.ch>
2014-10-17 13:23:30 +0200

    global jndi name aus properties

--- budget-swing/src/main/java/ch/tkayser/budget/swing/service/ServiceLocator.java
+++ budget-swing/src/main/java/ch/tkayser/budget/swing/service/ServiceLocator.java
@@ -1,5 +1,7 @@
 package ch.tkayser.budget.swing.service;
 
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.naming.InitialContext;
 
@@ -7,27 +9,39 @@ import ch.tkayser.budget.exception.BudgetException;
 import ch.tkayser.budget.service.BudgetService;
 
 public class ServiceLocator {
-	
-    private static BudgetService service;
-    
+
+    private static BudgetService  service;
+
     private static InitialContext context;
-    
+
+    private static final String   SERVICE_PROPERTY_FILE       = "service.properties";
+
+    private static final String   BUDGETSERVICE_JNDI_PROPERTY = "budgetservice.jndi";
+
     public static void init(LoginInformation loginInformation) throws BudgetException {
-        context = GlasfishLoginHandler.login(loginInformation);  
+        context = GlasfishLoginHandler.login(loginInformation);
         initService();
     }
-    
+
     public static BudgetService getService() {
-    	return service;
+        return service;
     }
 
-	private static void initService() throws BudgetException {
-		try {
-		       service = (BudgetService) context.lookup("java:global/budget-ear-2.0.1-SNAPSHOT/budget-server-2.0.1-SNAPSHOT/BudgetServiceImpl");	        			
-		} catch (Exception e) {
-			throw new BudgetException("Fehler beim Service lookup: "+e.getMessage(), e);
-		}
-	}
+    private static void initService() throws BudgetException {
+        try {
+            // get jndi name from file
+            // Basis properties aus jndi.properties file
+            Properties p = new Properties();
+            InputStream stream = ServiceLocator.class.getClassLoader().getResourceAsStream(SERVICE_PROPERTY_FILE);
+            if (stream == null) {
+                throw new BudgetException("jndi File not Found");
+            }
+            p.load(stream);
 
+            service = (BudgetService) context.lookup(p.getProperty(BUDGETSERVICE_JNDI_PROPERTY));
+        } catch (Exception e) {
+            throw new BudgetException("Fehler beim Service lookup: " + e.getMessage(), e);
+        }
+    }
 
 }
